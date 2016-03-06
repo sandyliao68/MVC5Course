f21449a2d197faadb726e206a09605e25a882838
sandy0124@gmail.com <sandy0124@gmail.com>
2016-03-06 10:38:54 +0800

    練習使用 FabricsEntities 物件服務新增一筆 Product 資料，並顯示在頁面上

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MVC5Course.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        // GET: EF
+        public ActionResult Index()
+        {
+            var db = new FabricsEntities();
+            db.Product..Add (new Product()
+            {
+                ProductName ="BMW",
+                Price=5,
+                Stock=1,
+                Active=true
+
+            });
+            db.SaveChanges();
+            var data=db.Product.ToList();
+
+
+            return View(data);
+        }
+    }
+}
---
+++ MVC5Course/Models/MemberViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class MemberViewModel
+    {
+        public int ID { get; set; }
+        [Required]        
+        public string  Name { get; set; }
+
+        [Required]
+        public string Birthday { get; set; }
+    }
+}
